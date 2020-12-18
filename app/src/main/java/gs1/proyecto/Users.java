55bd0ac7a662e217a2635c87c027d40ad41f6c21
Ayoze <Ayozemn@hotmail.com>
2020-12-18 21:04:28 +0000

    BASE DE DATOS AÑADIDA PARA USUARIOS, SOLO REPLICARLA PARA LO QUE INTERESE

---
+++ app/src/main/java/gs1/proyecto/Users.java
@@ -0,0 +1,71 @@
+package gs1.proyecto;
+
+public class Users {
+    private int id;
+    private String usuario, nombre, email, pass;
+
+    //Constructors
+
+
+    public Users(int id, String usuario, String nombre, String email, String pass) {
+        this.id = id;
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public Users() {
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", usuario='" + usuario + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
