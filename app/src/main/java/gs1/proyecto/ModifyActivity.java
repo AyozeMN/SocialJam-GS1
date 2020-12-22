66fb7f60318baf2e4bed29733330e6a437d1288e
Ayoze <Ayozemn@hotmail.com>
2020-12-22 14:29:05 +0000

    Añadido el cambio de nivel de alerta pero no implementado

--- app/src/main/java/gs1/proyecto/ModifyActivity.java
+++ app/src/main/java/gs1/proyecto/ModifyActivity.java
@@ -16,6 +16,8 @@ import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.android.gms.common.util.Strings;
+
 import java.net.SocketOption;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,7 +34,9 @@ public class ModifyActivity extends AppCompatActivity {
     private String level;
     private BaseDeDatos baseDeDatos;
     private List<Marcador> listaMarcadores;
+    private List<String> listaNombres;
     private ArrayAdapter adapterTodo;
+    private int listSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +53,12 @@ public class ModifyActivity extends AppCompatActivity {
         lv_todo.setOnItemClickListener((parent, view, position, id) -> {
             lv_todo.setSelection(position);
             getAlertLevel(position);
+            listSelected = position;
         });
 
-        bt_actualizar.setOnClickListener(v -> changeLevel());
+        /*bt_actualizar.setOnClickListener(view -> {
+            setAlertLevel(listSelected);
+        });*/
 
         showEverything(baseDeDatos);
     }
@@ -67,6 +74,20 @@ public class ModifyActivity extends AppCompatActivity {
         radioGroup.check(radioGroup.getChildAt(levels.get(level)).getId());
     }
 
+    private void setAlertLevel(int position) {
+        int selected = radioGroup.getCheckedRadioButtonId();
+
+        Map<Integer, String> levels = new HashMap<>();
+        levels.put(0, "green");
+        levels.put(1, "yellow");
+        levels.put(2, "orange");
+        levels.put(3, "red");
+        levels.put(4, "black");
+
+        String level = levels.get(selected);
+        listaMarcadores.get(position).setNivel(level);
+    }
+
     private void changeLevel() {
         int selectedId = radioGroup.getCheckedRadioButtonId();
         RadioButton radioButton = findViewById(selectedId);
@@ -76,11 +97,13 @@ public class ModifyActivity extends AppCompatActivity {
 
     private void showEverything(BaseDeDatos baseDeDatos) {
         listaMarcadores = baseDeDatos.getEverything();
-        List<String> nameList = new ArrayList();
+        List<String> tituloList = new ArrayList();
+
         for (Marcador m : listaMarcadores) {
-            nameList.add(m.getTitulo());
+            tituloList.add(m.getTitulo());
+            //listaNombres.add(m.getNombre());
         }
-        adapterTodo = new ArrayAdapter<>(ModifyActivity.this, android.R.layout.simple_list_item_1, nameList);
+        adapterTodo = new ArrayAdapter<>(ModifyActivity.this, android.R.layout.simple_list_item_1, tituloList);
         lv_todo.setAdapter(adapterTodo);
     }
 
